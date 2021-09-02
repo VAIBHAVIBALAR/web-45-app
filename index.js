7dd45acfeecaff2b8cf579a7ce8d9c0f7dab85e6
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-09-02 10:59:59 -0700

    add react app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 
@@ -12,12 +13,17 @@ server.use(express.json())
 
 const PORT = process.env.PORT || 5000
 server.use(cors())
+
+server.use(express.static(path.join(__dirname, 'client/build')))
 server.get('/', (req, res) => {
     res.send('<h1>Web-45</h1>')
 })
 
 server.get('/api', (req, res) => {
     res.json({ message: 'Web 45 is awesome'})
+})
+server.get('*', (req, res) =>{
+    res.sendFile(path.join(_dirname, 'client/build', 'index.html'))
 })
 
 server.listen(PORT, () => {
