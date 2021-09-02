715153033478d9fd75c2eb2321e0123e0662b7a5
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-09-02 11:11:17 -0700

    final1

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
+// this allows to stash "artificial env variables" in a file
 require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const cors = require('cors')
 
-console.log(process.env.USER)
-console.log(process.env.HOME)
-console.log(process.env.PORT)
+console.log(process.env.LADY)
+console.log(process.env.API_SECRET)
+
+const PORT = process.env.PORT || 5000 // fallback is nice
 
 const server = express()
+
 server.use(express.json())
-
-
-const PORT = process.env.PORT || 5000
 server.use(cors())
-
 server.use(express.static(path.join(__dirname, 'client/build')))
-server.get('/', (req, res) => {
-    res.send('<h1>Web-45</h1>')
-})
 
 server.get('/api', (req, res) => {
-    res.json({ message: 'Web 45 is awesome'})
+  res.json({ message: 'Web 45 is awesome!' })
 })
-server.get('*', (req, res) =>{
-    res.sendFile(path.join(_dirname, 'client/build', 'index.html'))
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 server.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
+  console.log(`listening on port ${PORT}`)
 })
