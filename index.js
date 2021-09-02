f145582adb4a40442c70bd97a927e7cd436a66c6
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-09-02 10:10:07 -0700

    new changes

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+console.log(process.env.USER)
+console.log(process.env.HOME)
+console.log(process.env.PORT)
+
+const server = express()
+server.use(express.json())
+
+
+const PORT = process.env.PORT || 5000
+server.use(cors())
+server.get('/', (req, res) => {
+    res.send('<h1>Web-45</h1>')
+})
+
+server.get('/api', (req, res) => {
+    res.json({ message: 'Web 45 is awesome'})
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
